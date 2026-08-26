36261f4b39a3a8e4028635a57e23516ed79c8920
Svyron <suvangandhi.p@gmail.com>
2026-08-26 15:44:04 +0000

    Implement Vec3 class and vector utility functions

--- learning-log.md
+++ learning-log.md
@@ -16,7 +16,7 @@
 
 ## Learned 
 
-- Guards: `#ifndef`, `#define`
+- Guards: `#ifndef`, `#define` , `#endif` 
 - `class` creates a custom type like example `double`, `int` etc 
 - Arrays and indexing 
 - Constructors 
@@ -25,3 +25,21 @@
 - `this` - address of the current object 
 - `*this` - the current object itself 
 - `&` - takes a copy and not the original (saves a lot of ram if used right) 
+- Operator overloading functions --> return type | operator | input(s)  
+// input --> thing being used in the operation with the current object 
+
+# DAY 3 - 26-08-2026 
+
+## Learned 
+
+- Header files contain declarations or interfaces that other files can use 
+- cpp files contain the actual implementation and are compiled seperately 
+- `#include "file.h"` → makes the header's contents available to that `.cpp` file. 
+- Header files are normally not compiled by themselves. 
+- Output stream - a path the data travels through when your program wants to send something out 
+- MENTAL MODEL --> RETURN TYPE | OPERATOR BEING DEFINED | LEFT INPUT | RIGHT INPUT 
+- (operator+= was used to modify vector 1 where vector 2 remains the same but operator+ is used to add to vectors to produce a new result) 
+
+### Member functions and non-member functions 
+- Member functions are defined inside the class. This is cause member functions get the current object automatically through `this` 
+- Non-member functioins are defined outside the class. They dont have `this` for vec3, so (say for example a+b) both a→u and a→v are passed explicitly. 
--- src/vec3.h
+++ src/vec3.h
@@ -4,6 +4,7 @@
 #include <cmath> 
 #include <iostream> 
 
+// defining a class called vec3 to represent a 3D vector with x, y, z components and various operations that can be performed on it.
 class vec3 
 {
     public:
@@ -18,18 +19,104 @@ class vec3
 
         vec3 operator-() const { return vec3(-e[0], -e[1], -e[2]); } // teaching the existing operator (-) to the vec3 class to negate the vector 
 
-        double operator[](int i) const { return e[i]; } // teaching the exisitng operator ([]) to the vec3 class to access the elements of the vector // "Give me a COPY of the double."
+        // teaching the exisitng operator ([]) to the vec3 class to access the elements of the vector // "Give me a COPY of the double."
+        double operator[](int i) const { return e[i]; } 
 
-        double& operator[](int i) { return e[i]; } // teaching the existing operator ([]) to the vec3 class to access the elements of the vetor and MODIFY them // "Give me the ORIGINAL double itself, by reference."
+        // teaching the existing operator ([]) to the vec3 class to access the elements of the vetor and MODIFY them 
+        // "Give me the ORIGINAL double itself, by reference."
+        double& operator[](int i) { return e[i]; } 
 
-    vec3& operator+=(const vec3& v) // teaching the existing operator (+=) to the vec3 class to add another vector 'v' to the vector and modify the vector 
-    {
-        e[0] += v.e[0];
-        e[1] += v.e[1];
-        e[2] += v.e[2];
-        return *this; // return the modified vector // 'this' shows the address of the current object while '*this' dereferences the pointer to get the object itself.
-    }
+        // teaching the existing operator (+=) to the vec3 class to add another vector 'v' to the vector and modify the vector
+        vec3& operator+=(const vec3& v)  
+        {
+            e[0] += v.e[0];
+            e[1] += v.e[1];
+            e[2] += v.e[2];
+            return *this; // return the modified vector // 'this' shows the address of the current object while '*this' dereferences the pointer to get the object itself.
+        }
     
+        // teaching the existing operator (*=) to the vec3 class to multiply the vector by a scalar 't' and modify the vector 
+        vec3& operator*=(const double t) 
+        {
+            e[0] *= t; 
+            e[1] *= t; 
+            e[2] *= t; 
+            return *this; // return the modified vector 
+        } 
+        
+        // teaching the existing operator (/=) to the vec3 class to divide the vector by a scalar 't' and modify the vector 
+        vec3& operator /=(const double t) 
+        {
+            return *this *= 1/t; // return the modified vector 
+        } 
+
+        double length() const // to calculate the length of the vector 
+        {
+            return std::sqrt(length_squared()); // length_squared() is defined below and std::sqrt() is used to calculate the square root of the length_squared()
+        } 
+        
+        double length_squared() const 
+        {
+            return e[0]*e[0] + e[1]*e[1] + e[2]*e[2]; // return the sum of squares of the x, y, z values of the vector
+        }
 }; 
+
+// point3 is an alias of vec3, it represents a point in the 3D space // used an alias cause both use the same data structure but have different meanings, so to avoid confusion, we use an alias 
+using point3 = vec3;
+
+// teaching the existing operator (<<) to the vec3 class to print the vector to the console 
+// left side of comma is the output stream, right side is the vector to be printed 
+inline std::ostream& operator<<(std::ostream& out, const vec3& v)
+{
+    return out << v.e[0] << ' ' << v.e[1] << ' ' << v.e[2]; // print the x, y, z values of the vector to the console 
+} 
+
+// teaching the existing operator (+) to the vec3 class to add two vectors and return the result as a new vector 
+inline vec3 operator+(const vec3& u, const vec3& v) 
+{
+    return vec3(u.e[0] + v.e[0], u.e[1] + v.e[1], u.e[2] + v.e[2]); // return a new vector with the sum of the x, y, z values of the two vectors 
+}
+
+inline vec3 operator-(const vec3& u, const vec3& v) 
+{
+    return vec3(v.e[0] - u.e[0], v.e[1] - u.e[1], v.e[2] - u.e[2]); // return a new vector with the difference of the x, y, z values of the two vectors 
+}
+
+inline vec3 operator*(const vec3& u, const vec3& v) 
+{
+    return vec3(u.e[0] * v.e[0], u.e[1] * v.e[1], u.e[2] * v.e[2]); 
+}
+
+inline vec3 operator*(double t, const vec3& v) 
+{
+    return vec3(t * v.e[0], t * v.e[1], t * v.e[2]); 
+}
+
+inline vec3 operator*(const vec3& v, double t) 
+{
+    return t * v; 
+}
+
+inline vec3 operator/(const vec3& v, double t) 
+{
+    return v * (1/t);  
+}
+
+inline double dot(const vec3& u, const vec3& v) 
+{
+    return u.e[0] * v.e[0] + u.e[1] * v.e[1] + u.e[2] * v.e[2]; 
+}
+
+inline vec3 cross(const vec3& u, const vec3& v) 
+{
+    return vec3(u.e[1] * v.e[2] - u.e[2] * v.e[1],
+                u.e[2] * v.e[0] - u.e[0] * v.e[2],
+                u.e[0] * v.e[1] - u.e[1] * v.e[0]); 
+}
+
+inline vec3 unit_vector(const vec3& v) 
+{
+    return v / v.length(); 
+}
 
 #endif 
